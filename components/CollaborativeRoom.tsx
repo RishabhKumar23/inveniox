2bb85c48477ecd1b7c249d0f8a04c31028cd836a
Rishabh Kumar <112078029+RishabhKumar23@users.noreply.github.com>
2024-11-18 12:08:31 +0000

    Dynamic Document Title
    User can change dynamically Document's Title.

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { ClientSideSuspense, RoomProvider } from '@liveblocks/react/suspense'
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useEffect, useRef, useState } from 'react'
 import Loader from './Loader'
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
@@ -11,16 +11,113 @@ import {
     UserButton
 } from '@clerk/nextjs'
 import ActiveCollaborators from './ActiveCollaborators'
+import { Input } from './ui/input'
+import { currentUser } from '@clerk/nextjs/server'
+import Image from 'next/image'
+import { updateDocument } from '@/lib/actions/room.actions'
 
-const CollaborativeRoom = ({roomId, roomMetadata}: CollaborativeRoomProps) => {
+const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => {
+    //TODO - Later we make it dynamic
+    const currentUserType = 'editor';
+
+    // To edit Document Title
+    const [documentTitle, setdocumentTitle] = useState(roomMetadata.title);
+    const [editing, setEditing] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const containerRef = useRef<HTMLDivElement>(null);
+    const inputRef = useRef<HTMLDivElement>(null);
+
+    // Method to update or track each keyboard key and store
+    const updateTitleHandler = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key == "Enter") {
+            setLoading(true);
+
+            try {
+                if (documentTitle !== roomMetadata.title) {
+                    const updatedDocument = await updateDocument(roomId, documentTitle)
+
+                    if (updatedDocument) {
+                        setEditing(false);
+                    }
+                }
+            } catch (error) {
+                console.error(error)
+            }
+
+            setLoading(false);
+        }
+    }
+
+    // recall's every time we change roomId or documentTitle
+    useEffect(() => {
+        // If we click the mouse outside the editing area we save the Title
+        const handleClickOutside = (e: MouseEvent) => {
+            if (containerRef.current && !containerRef.current.contains(e.target as Node)) {
+                setEditing(false);
+                updateDocument(roomId, documentTitle);
+            }
+        }
+
+        // if mouse button press down call the handleClickOutside method
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+
+    }, [roomId, documentTitle]);
+
+    // recall's every time we are editing
+    useEffect(() => {
+        if (editing && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [editing]);
+
     return (
         <RoomProvider id={roomId}>
             <ClientSideSuspense fallback={<Loader />}>
                 <div className='collaborative-room'>
                     <Header>
                         {/* for share button  */}
-                        <div className='flex w-fit items-center justify-center gap-2'>
-                            <p className='document-title'>Share</p>
+                        <div ref={containerRef} className='flex w-fit items-center justify-center gap-2'>
+                            {/* <p className='document-title'>Share</p> */}
+                            {editing && !loading ? (
+                                <Input
+                                    type='text'
+                                    value={documentTitle}
+                                    ref={inputRef}
+                                    placeholder='Enter Title'
+                                    onChange={(e) => setdocumentTitle(e.target.value)}
+                                    onKeyDown={updateTitleHandler}
+                                    disabled={!editing}
+                                    className='document-title-input'
+                                />
+                            ) : (
+                                <>
+                                    <p className='document-title'>{documentTitle}</p>
+                                </>
+                            )}
+
+                            {/* If Not editing the document title */}
+                            {currentUserType === 'editor' && !editing && (
+                                <Image
+                                    src="/assets/icons/edit.svg"
+                                    alt='Edit'
+                                    width={24}
+                                    height={24}
+                                    onClick={() => setEditing(true)}
+                                    className='pointer'
+                                />
+                            )}
+                            {/* If editing the document title */}
+                            {currentUserType !== 'editor' && !editing && (
+                                <p className='view-only-tag'>View Only</p>
+                            )}
+
+                            {loading && <p className='text-sm text-gray-400'>saving...</p>}
+
                         </div>
                         {/* */}
                         <div className='flex w-full flex-1 justify-end gap-2 sm:gap-3'>
