72df45e33a4e80252b13d747f5f7077893748beb
Rishabh Kumar <112078029+RishabhKumar23@users.noreply.github.com>
2024-11-20 21:25:08 +0000

    Share Modal Added
    Now user can share their document to others through email id's

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -15,6 +15,7 @@ import { Input } from './ui/input'
 import { currentUser } from '@clerk/nextjs/server'
 import Image from 'next/image'
 import { updateDocument } from '@/lib/actions/room.actions'
+import ShareModal from './ShareModal'
 
 const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: CollaborativeRoomProps) => {
     //TODO - Later we make it dynamic
@@ -122,6 +123,13 @@ const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: Col
                         {/* */}
                         <div className='flex w-full flex-1 justify-end gap-2 sm:gap-3'>
                             <ActiveCollaborators />
+                            <ShareModal
+                                roomId={roomId}
+                                collaborators={users}
+                                creatorId={roomMetadata.creatorId}
+                                currentUserType={currentUserType}
+
+                            />
                             <SignedOut>
                                 <SignInButton />
                             </SignedOut>
