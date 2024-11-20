e1ca96ec63e523e14173f05eb84c6eb0aca92946
Rishabh Kumar <112078029+RishabhKumar23@users.noreply.github.com>
2024-11-20 17:06:39 +0000

    Mention Feature
    User can Mention collaborators to the comments

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -5,15 +5,28 @@ import {
     ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import Loader from '@/components/Loader';
-import { getClerkUsers } from '@/lib/actions/user.action';
+import { getClerkUsers, getDocumentUsers } from '@/lib/actions/user.action';
+import { useClerk } from '@clerk/nextjs';
 
 const Provider = ({ children }: { children: ReactNode }) => {
+
+    const { user: clerkUser } = useClerk();
+
     return (
         <LiveblocksProvider
             authEndpoint="/api/liveblocks-auth"
             resolveUsers={async ({ userIds }) => {
                 const users = await getClerkUsers({ userIds });
                 return users;
+            }}
+            resolveMentionSuggestions={async ({ text, roomId }) => {
+                const roomUsers = await getDocumentUsers({
+                    roomId,
+                    currentUser: clerkUser?.emailAddresses[0].emailAddress,
+                    text,
+                })
+
+                return roomUsers;
             }}
         >
             {/* <RoomProvider id="my-room"> */}
